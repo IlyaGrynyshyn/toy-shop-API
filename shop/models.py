80cf5979f5af9c261c3fa0344e1beca62e9ef03e
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-29 20:51:58 +0200

    Implement Prodcut model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Model responsible for products
+    """
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"product_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ["-id"]
