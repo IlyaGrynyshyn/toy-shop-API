708fc3c39e5e6ac3cc6702662fa5a3686df895ce
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-29 21:29:37 +0200

    Implement view set for product

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from shop.models import Product
+from shop.serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "slug"
