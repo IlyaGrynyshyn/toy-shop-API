69e7c36b0fe9bda2fa9de34361c663a248d865b8
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-29 21:46:04 +0200

    Wrapp IsAdminUserOrReadOnly in a shower

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from shop.models import Product
+from shop.permissions import IsAdminUserOrReadOnly
 from shop.serializers import ProductSerializer
 
 
@@ -8,3 +9,4 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "slug"
+    permission_classes = [IsAdminUserOrReadOnly]
