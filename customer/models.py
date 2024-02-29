2ab636232a0676c8bb793e6ca3054f8f75e6ceee
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-02-29 16:29:06 +0200

    Imelement login by email instend username

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from customer.managers import UserManager
+
+
+class Customer(AbstractUser):
+
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
