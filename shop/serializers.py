dbbecf9aab6ecaa8576a9eb0a9646c33f01e66a4
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-01 11:01:50 +0200

    Make field slug read only in ProductSerializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -4,7 +4,9 @@ from shop.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+
     class Meta:
-        fields = ("id", "title", "slug", "price")
+        fields = ("id", "title", "slug", "price", "image")
         model = Product
         lookup_field = "slug"
