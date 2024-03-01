ba7ffb1c99975149eafd07b77233f7f38aaa753f
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-01 13:47:14 +0200

    Change path for product images

--- shop/models.py
+++ shop/models.py
@@ -10,7 +10,7 @@ def movie_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/products/", filename)
 
 
 class Product(models.Model):
