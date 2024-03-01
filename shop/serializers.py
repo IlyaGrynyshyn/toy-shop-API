7f8c76e0476264357ba1d6521e7eb1f2f1044f14
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-01 18:09:14 +0200

    Implement category serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,21 +1,28 @@
 from rest_framework import serializers
 
-from shop.models import Product
+from shop.models import Product, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ("id", "title", "slug")
+        lookup_field = "slug"
 
 
 class ProductSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only=True)
 
     class Meta:
-        fields = ("id", "title", "slug", "price", "image")
+        fields = ("id", "title", "category", "slug", "price", "image")
         model = Product
         lookup_field = "slug"
 
 
-class ProdcuctListSerializer(ProductSerializer): ...
-
-
-class ProdcutDetailSerializer(ProductSerializer): ...
+class ProdcutDetailSerializer(ProductSerializer):
+    category = CategorySerializer()
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
