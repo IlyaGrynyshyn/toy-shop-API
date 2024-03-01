9e792462f1276f9f7d34958569dc657712c196e6
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-01 22:00:00 +0200

    Implement order by price

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
@@ -22,10 +23,18 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "slug"
     permission_classes = [IsAdminUserOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        sort_by_price = self.request.query_params.get("sort")
+        if sort_by_price == "price_asc":
+            queryset = queryset.order_by("price")  # sort by price down from 1-10
+        if sort_by_price == "price_desc":
+            queryset = queryset.order_by("-price")  # sort by price from 10-1
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -50,3 +59,15 @@ class ProductViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "sort",
+                type={"type": "string", "items": {"type": "text"}},
+                description="Sort by price down use 'price_desc'. Sort by price up use 'price_asc' ",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
