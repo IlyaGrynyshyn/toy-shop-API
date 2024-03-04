b7f821f489b28624d4bd2293286fa60cf80cd48b
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-04 19:42:37 +0200

    Add corsheader in installapp

--- toy_shop_api/settings.py
+++ toy_shop_api/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "drf_spectacular",
     "customer",
@@ -171,6 +172,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:3080",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
