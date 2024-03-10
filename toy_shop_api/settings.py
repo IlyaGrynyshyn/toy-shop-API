3e9f02bb46983d57b56cced8e639399aaa65267d
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-10 18:31:38 +0200

    Add localhost in ALLOWED_HOSTS

--- toy_shop_api/settings.py
+++ toy_shop_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
