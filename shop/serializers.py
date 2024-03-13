7369f98aab4f114175970f42cf90d5ce38f6b2f1
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-13 18:27:23 +0200

    Implement product image serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from shop.models import Product, Category
+from shop.models import Product, Category, ProductImage
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -9,23 +9,44 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ("id", "title", "slug")
-        lookup_field = "slug"
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductImage
+        fields = ("id", "product", "image")
 
 
 class ProductSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(
+            max_length=1000000, allow_empty_file=False, use_url=False
+        ),
+        write_only=True,
+    )
 
     class Meta:
-        fields = ("id", "title", "category", "slug", "price", "image")
         model = Product
-        lookup_field = "slug"
+        fields = (
+            "id",
+            "title",
+            "category",
+            "images",
+            "slug",
+            "price",
+            "uploaded_images",
+        )
+
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images")
+        product = Product.objects.create(**validated_data)
+        for image in uploaded_images:
+            ProductImage.objects.create(product=product, image=image)
+        return product
 
 
-class ProdcutDetailSerializer(ProductSerializer):
+class ProductDetailSerializer(ProductSerializer):
     category = CategorySerializer()
-
-
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ("id", "image")
