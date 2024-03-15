9b66184548fb6cba1af8000571490bedc7f08e13
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-15 18:00:52 +0200

    Change DEBUG settings

--- toy_shop_api/settings.py
+++ toy_shop_api/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+# DEBUG = "RENDER" not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
