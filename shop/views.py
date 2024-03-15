ddfa883275c44c699a48c89579057cf7f18ca01d
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-15 17:58:30 +0200

    Remove unnecessary imports

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,5 @@
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 
 from shop.models import Product, Category, ProductImage, Material
