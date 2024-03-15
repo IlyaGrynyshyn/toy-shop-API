39c132c3871bb276e70240f4829a56213eeeee36
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-15 19:36:07 +0200

    Implement OrderViewSet

--- order/views.py
+++ order/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from order.models import Order
+from order.serializers import OrderSerializer
+from shop.permissions import IsAdminUserOrReadOnly
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
