2284168eed2732bae80b08e3154a01a78322989a
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-15 19:30:20 +0200

    Implement Order & OrderItem models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from shop.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_items"
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return "{}".format(self.id)
+
+    def get_cost(self):
+        """
+        Calculates the total cost of the order item
+        """
+        return self.price * self.quantity
