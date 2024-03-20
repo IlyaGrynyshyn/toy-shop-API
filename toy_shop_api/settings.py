2dddca01e85b0469637b6d8150cf66327358ab15
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-20 16:55:15 +0200

    Media settings

--- toy_shop_api/settings.py
+++ toy_shop_api/settings.py
@@ -148,13 +148,10 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media").replace("\\", "/")
-
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
 
 AUTH_USER_MODEL = "customer.Customer"
 
