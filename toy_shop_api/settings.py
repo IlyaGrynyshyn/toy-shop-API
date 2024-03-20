df03a2b0d82c505702a86450ce246e6816114cd6
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-03-20 16:30:01 +0200

    Change media_root settings

--- toy_shop_api/settings.py
+++ toy_shop_api/settings.py
@@ -148,7 +148,8 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media").replace("\\", "/")
