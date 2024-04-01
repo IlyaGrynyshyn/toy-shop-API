168a81798d89c35963585a0831affecf4675d770
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-02 00:08:04 +0300

    Merge base.py

--- toy_shop_api/settings/base.py
+++ toy_shop_api/settings/base.py
@@ -31,6 +31,19 @@ SECRET_KEY = os.getenv(
 )
 
 
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = "RENDER" not in os.environ
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+# Application definition
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
