038da772d109b2a440cf19e3e0b9047527db24c7
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-10 16:10:50 +0300

    Implement serializer for cartinformation

--- order/serializers.py
+++ order/serializers.py
@@ -1,7 +1,49 @@
 from django.db import transaction
 from rest_framework import serializers
+import datetime
 
 from order.models import Order, OrderItem
+
+
+class CardInformationSerializer(serializers.Serializer):
+    @staticmethod
+    def check_expiry_month(value):
+        if not 1 <= int(value) <= 12:
+            raise serializers.ValidationError("Invalid expiry month.")
+
+    @staticmethod
+    def check_expiry_year(value):
+        today = datetime.datetime.now()
+        if not int(value) >= today.year:
+            raise serializers.ValidationError("Invalid expiry year.")
+
+    @staticmethod
+    def check_cvc(value):
+        if not 3 <= len(value) <= 4:
+            raise serializers.ValidationError("Invalid cvc number.")
+
+    @staticmethod
+    def check_payment_method(value):
+        payment_method = value.lower()
+        if payment_method not in ["card"]:
+            raise serializers.ValidationError("Invalid payment_method.")
+
+    card_number = serializers.CharField(max_length=150, required=True)
+    expiry_month = serializers.CharField(
+        max_length=150,
+        required=True,
+        validators=[check_expiry_month],
+    )
+    expiry_year = serializers.CharField(
+        max_length=150,
+        required=True,
+        validators=[check_expiry_year],
+    )
+    cvc = serializers.CharField(
+        max_length=150,
+        required=True,
+        validators=[check_cvc],
+    )
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
