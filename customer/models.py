9ffc271549cd9a2c2c8c6e0ba75df5eb2517ec15
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-11 18:41:34 +0300

    Add field registration method  in model Customer

--- customer/models.py
+++ customer/models.py
@@ -6,9 +6,18 @@ from customer.managers import UserManager
 
 
 class Customer(AbstractUser):
+    REGISTRATION_CHOICES = [
+        ('email', 'Email'),
+        ('google', 'Google'),
+    ]
 
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    registration_method = models.CharField(
+        max_length=10,
+        choices=REGISTRATION_CHOICES,
+        default='email'
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
