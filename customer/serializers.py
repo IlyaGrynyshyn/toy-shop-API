b44520aec8e6a41e7e411d6f03aa67c4383489c2
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 20:49:42 +0300

    Implement serializer for change password

--- customer/serializers.py
+++ customer/serializers.py
@@ -20,3 +20,42 @@ class CustomerSerializer(serializers.ModelSerializer):
             user.set_password(password)
             user.save()
         return user
+
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ("old_password", "password", "password2")
+
+    def validate(self, attrs):
+        """
+        Validate that the password and password2 match
+        :param attrs:
+        :return:
+        """
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return attrs
+
+    def validate_old_password(self, value):
+        """
+        Validate old password against current password
+        """
+        user = self.context["request"].user
+        if not user.check_password(value):
+            raise serializers.ValidationError(
+                {"old_password": "Old password is not correct"}
+            )
+        return value
+
+    def update(self, instance, validated_data):
+        """
+        Update user password with new password
+        """
+        instance.set_password(validated_data["password"])
+        instance.save()
+        return instance
