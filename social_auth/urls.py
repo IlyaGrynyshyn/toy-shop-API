4614935d29f1fab5da085db285abb0eb45ebb29b
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 15:56:09 +0300

    Implement url for facebook auth

--- social_auth/urls.py
+++ social_auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from social_auth.views import GoogleSocialAuthView
+from social_auth.views import GoogleSocialAuthView, FacebookSocialAuthView
 
 app_name = "social_auth"
 
 urlpatterns = [
     path("google/", GoogleSocialAuthView.as_view(), name="google"),
+    path("facebook/", FacebookSocialAuthView.as_view(), name="facebook"),
 ]
