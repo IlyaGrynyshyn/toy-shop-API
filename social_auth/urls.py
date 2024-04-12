3145fe16ff1f2311b174bace03b453b386fc74bb
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 15:28:30 +0300

    Implement url for google auth

---
+++ social_auth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from social_auth.views import GoogleSocialAuthView
+
+app_name = "social_auth"
+
+urlpatterns = [
+    path("google/", GoogleSocialAuthView.as_view(), name="google"),
+]
