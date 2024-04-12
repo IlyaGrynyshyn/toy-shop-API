37a5f062361559b0c680b1de85dc92aa1aac4a98
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 15:56:55 +0300

    Implement tokens method for customer model

--- customer/models.py
+++ customer/models.py
@@ -19,3 +19,10 @@ class Customer(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {"refresh": str(refresh), "access": str(refresh.access_token)}
