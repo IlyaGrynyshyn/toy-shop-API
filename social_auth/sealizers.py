1fca79564037a17f8ce3f39751b4581185e53d91
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 15:50:30 +0300

    Implement facebook auth serializer

--- social_auth/sealizers.py
+++ social_auth/sealizers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.conf import settings
 
-from social_auth import google
+from social_auth import google, facebook
 
 from social_auth.register import register_social_user
 
@@ -9,7 +9,15 @@ from social_auth.register import register_social_user
 class GoogleSocialAuthSerializer(serializers.Serializer):
     auth_token = serializers.CharField()
 
-    def validate(self, auth_token):
+    def validate(self, auth_token: str) -> dict:
+        """
+        Validates the Google authentication token.
+
+        :param auth_token: The authentication token to be validated.
+        :return: A dictionary with information about the registered user
+
+        raise: serializers.ValidationError: If the token is invalid, expired, or does not match.
+        """
         user_data = google.Google.validate(auth_token)
         try:
             user_data["sub"]
@@ -24,3 +32,26 @@ class GoogleSocialAuthSerializer(serializers.Serializer):
         provider = "google"
 
         return register_social_user(provider=provider, email=email)
+
+
+class FacebookSocialAuthSerializer(serializers.Serializer):
+    auth_token = serializers.CharField()
+
+    def validate_auth_token(self, auth_token: str) -> dict:
+        """
+        Validates the Facebook authentication token.
+
+        :param auth_token: The authentication token to be validated.
+        :return: A dictionary with information about the registered user
+
+        :raise: serializers.ValidationError: If the token is invalid or expired.
+        """
+        user_data = facebook.Facebook.validate(auth_token)
+        try:
+            email = user_data["email"]
+            provider = "facebook"
+            return register_social_user(provider=provider, email=email)
+        except:
+            raise serializers.ValidationError(
+                "The token  is invalid or expired. Please login again."
+            )
