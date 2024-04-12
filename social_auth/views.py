eb0b558af92b90e4ddcd671216d98cc7ae800819
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 15:26:00 +0300

    Implement view for google auth

--- social_auth/views.py
+++ social_auth/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from social_auth.sealizers import GoogleSocialAuthSerializer
+
+
+class GoogleSocialAuthView(generics.GenericAPIView):
+    serializer_class = GoogleSocialAuthSerializer
+
+    def post(self, request, *args, **kwargs) -> Response:
+        """
+        Send an idtoken as from google to get user information.
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = ((serializer.validated_data)["auth_token"])
+        return Response(data, status=status.HTTP_200_OK)
