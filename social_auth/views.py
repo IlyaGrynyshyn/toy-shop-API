bb0c566c5c065ff12f4950b8e5549eb4fd853d74
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-12 15:55:56 +0300

    Implement view for facebook auth

--- social_auth/views.py
+++ social_auth/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from social_auth.sealizers import GoogleSocialAuthSerializer
+from social_auth.sealizers import (
+    GoogleSocialAuthSerializer,
+    FacebookSocialAuthSerializer,
+)
 
 
 class GoogleSocialAuthView(generics.GenericAPIView):
@@ -10,6 +13,19 @@ class GoogleSocialAuthView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs) -> Response:
         """
         Send an idtoken as from google to get user information.
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = (serializer.validated_data)["auth_token"]
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class FacebookSocialAuthView(generics.GenericAPIView):
+    serializer_class = FacebookSocialAuthSerializer
+
+    def post(self, request, *args, **kwargs) -> Response:
+        """
+        Send an access token as from facebook to get user information.
         """
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
