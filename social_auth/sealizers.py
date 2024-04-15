a6933e6044f56d2dc77a731b3aee5a492074dedf
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-15 23:02:40 +0300

    Fix bug "Not work auth by google"

--- social_auth/sealizers.py
+++ social_auth/sealizers.py
@@ -1,5 +1,8 @@
+import os
+
 from rest_framework import serializers
 from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
 
 from social_auth import google, facebook
 
@@ -9,29 +12,28 @@ from social_auth.register import register_social_user
 class GoogleSocialAuthSerializer(serializers.Serializer):
     auth_token = serializers.CharField()
 
-    def validate(self, auth_token: str) -> dict:
-        """
-        Validates the Google authentication token.
-
-        :param auth_token: The authentication token to be validated.
-        :return: A dictionary with information about the registered user
-
-        raise: serializers.ValidationError: If the token is invalid, expired, or does not match.
-        """
+    def validate_auth_token(self, auth_token):
         user_data = google.Google.validate(auth_token)
+        print(user_data)
         try:
             user_data["sub"]
         except:
             raise serializers.ValidationError(
-                "The token is invalid or expired. Please try again."
+                "The token is invalid or expired. Please login again."
             )
-        if user_data["sub"] != settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY:
-            raise serializers.ValidationError("oops, Who are you?")
+
+        if user_data["aud"] != os.environ.get("GOOGLE_CLIENT_ID"):
+
+            raise AuthenticationFailed("oops, who are you?")
 
         email = user_data["email"]
+        first_name = user_data["given_name"]
+        last_name = user_data["family_name"]
         provider = "google"
 
-        return register_social_user(provider=provider, email=email)
+        return register_social_user(
+            provider=provider, email=email, first_name=first_name, last_name=last_name
+        )
 
 
 class FacebookSocialAuthSerializer(serializers.Serializer):
